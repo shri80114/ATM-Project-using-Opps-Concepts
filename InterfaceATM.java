4bc98a6d4177340f79b9183fb83dbbc942ddfa5b
shri80114 <112969204+shri80114@users.noreply.github.com>
2022-11-29 12:31:56 +0530

    Add files via upload

---
+++ InterfaceATM.java
@@ -0,0 +1,10 @@
+package Project;
+
+public interface InterfaceATM 
+{
+	public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+
+}
